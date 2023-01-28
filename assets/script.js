e99f343779b81f098959611ffe45bdec66105835
Jacob Boyce <jboyce313@gmail.com>
2023-01-28 10:45:59 -0600

    added movies to array

--- assets/script.js
+++ assets/script.js
@@ -4,7 +4,6 @@ var movieImage = $("#movie-image");
 var score = 0;
 var rottenTomatoesScore;
 
-
 var movies = [
   "tt0099700", // gremlins 2
   "tt0133093", // the matrix
@@ -50,12 +49,74 @@ var movies = [
   "tt0247638", // the princess diaries
   "tt0243655", // wet hot american summer
   "tt0120591", // armageddon
+  "tt3704428", // elvis
+  "tt0092099", // top gun
+  "tt0286112", // shaolin soccer
+  "tt0380510", // the lovely bones
+  "tt0212985", // hannibal
+  "tt0068646", // the godfather
+  "tt7991608", // red notice
+  "tt8946378", // knives out
+  "tt2527338", // the rise of skywalker
+  "tt0076759", // star wars
+  "tt1160419", // dune
+  "tt0145487", // spider-man
+  "tt0111257", // speed
+  "tt0118880", // con air
+  "tt0118571", // air force one
+  "tt0822832", // marley and me
+  "tt1781922", // no escape
+  "tt0357413", // anchorman
+  "tt0094137", // three men and a baby
+  "tt0811080", // speed racer
+  "tt0108255", // super mario bros
+  "tt0086567", // war games
+  "tt1677720", // ready player one
+  "tt0780504", // drive
+  "tt0032553", // the great dictator
+  "tt0098384", // steel magnolias
+  "tt0120693", // half baked
+  "tt0094898", // coming to america
+  "tt0368226", // the room
+  "tt0903624", // the hobbit
+  "tt0443453", // borat
+  "tt0473075", // prince of persia
+  "tt3861390", // dumbo
+  "tt0144084", // american psycho
+  "tt0163651", // american pie
+  "tt0277371", // not another teen movie
+  "tt0445934", // blades of glory
+  "tt0374900", // napolean dynamite
+  "tt0114709", // toy story
+  "tt0949731", // the happening
+  "tt2911666", // john wick
+  "tt0936501", // taken
+  "tt5433138", // f9
+  "tt1099212", // twilight
+  "tt0097351", // field of dreams
+  "tt0108037", // the sandlot
+  "tt0187393", // the patriot
+  "tt0183790", // a knights tale
+  "tt0087332", // ghostbuster
+  "tt0056592", // to kill a mockingbird
+  "tt1130884", // shutter island
+  "tt1179904", // paranormal activity
+  "tt0246578", // donnie darko
+  "tt0377092", // mean girls
+  "tt1478338", // bridesmaids
+  "tt0387808", // idiocracy
+  "tt0076666", // saturday night fever
+  "tt4975722", // moonlight
+  "tt0089927", // rocky 4
+  "tt0086960", // beverly hills cop
+  "tt2543164", // arrival
+  "tt0449010", // eragon
+  "tt0096895", // batman (1989)
+  "tt0107688", // nightmare before christmas
 ];
 
 var index = generateIndex();
 loadMovie(movies[index]);
-
-
 
 function loadMovie(movieID) {
   if (movies.length === 0) {
@@ -68,10 +129,9 @@ function loadMovie(movieID) {
       console.log(data);
       movieImage.attr("src", data.Poster);
       rottenTomatoesScore = parseInt(data.Ratings[1].Value);
-      var dataTitle = data.Title.replace(/ /g, "%20")
-      console.log(dataTitle)
-      call(dataTitle)
-    
+      var dataTitle = data.Title.replace(/ /g, "%20");
+      console.log(dataTitle);
+      call(dataTitle);
     });
 }
 
@@ -116,19 +176,19 @@ function checkGuess(guess) {
   }
 }
 
-
-function call(dataTitle){$.ajax({
-  url: "https://en.wikipedia.org/w/api.php?action=opensearch&search="+dataTitle+ "&limit=10&namespace=0&format=json&origin=*",
-  success: function(result) {
-     var wikiData = result
-    console.log(wikiData)
-    console.log(dataTitle)
-
-  
-
-
-  }})}
-
+function call(dataTitle) {
+  $.ajax({
+    url:
+      "https://en.wikipedia.org/w/api.php?action=opensearch&search=" +
+      dataTitle +
+      "&limit=10&namespace=0&format=json&origin=*",
+    success: function (result) {
+      var wikiData = result;
+      console.log(wikiData);
+      console.log(dataTitle);
+    },
+  });
+}
 
 function kippsButton() {
   window.open("https://github.com/kcavner", "_blank");
@@ -145,4 +205,3 @@ function jacobsButton() {
 function startGame() {
   window.location.href = "./score-guesser.html";
 }
-
