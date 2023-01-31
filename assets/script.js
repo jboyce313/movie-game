9520a0763f413909f08eca9e4d95bc2acb86e775
Casen Sutherland <117950906+casensutherland@users.noreply.github.com>
2023-01-30 20:32:06 -0600

    gameplay page update

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,24 @@
+// Bulma slider function
+function findOutputForSlider(el) {
+  var idVal = el.id;
+  outputs = document.getElementsByTagName('output');
+  for( var i = 0; i < outputs.length; i++ ) {
+    if (outputs[i].htmlFor == idVal)
+      return outputs[i];
+  }
+}
+// Bulma slider function
+var sliders = document.querySelectorAll( 'input[type="range"].slider' );
+[].forEach.call( sliders, function ( slider ) {
+ var output = findOutputForSlider( slider );
+ if ( output ) {
+   slider.addEventListener( 'input', function( event ) {
+     output.value = event.target.value;
+   } );
+ }
+} );
+
+
 var submitBtn = $(".submit");
 var guessInput = $(".input");
 var movieImage = $("#moviePoster");
